427def266604958b2698e67caca320a2e164de44
Angga <anggariskysetiawan@gmail.com>
2022-09-23 12:49:22 +0700

    first commit

---
+++ lib/styles/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color whiteSoft = Color(0xffF5F5F5);
+Color purpleSoft = Color(0xffF0EFF4);
+Color grey = Color(0xffB1B1B1);
+Color black = Color(0xff1C1C27);
+Color white = Color(0xffffffff);
